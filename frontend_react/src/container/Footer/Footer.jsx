d1d1f778a94513bfd774b41d669a09f52846e0a0
Phuc Mai <mariamaihong1309@gmail.com>
2023-04-01 21:48:57 -0400

    second release

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -42,7 +42,7 @@ const Footer = () => {
             <div className="app__footer-cards">
                 <div className="app__footer-card ">
                     <img src={images.email} alt="email" />
-                    <a href="mariamaihong1309@gmail.com" className="p-text">mariamaihong1309@gmail.com</a>
+                    <a href="mailto:mariamaihong1309@gmail.com" className="p-text">mariamaihong1309@gmail.com</a>
                 </div>
                 <div className="app__footer-card">
                     <img src={images.mobile} alt="phone" />
